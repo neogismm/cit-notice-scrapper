9b41fe74a9fe45afb014a1b2fedaf3c7cb380f08
Pritam Neog <neog.geek123@gmail.com>
2022-08-14 20:25:26 +0530

    mail.py

---
+++ mail.py
@@ -0,0 +1,10 @@
+import app
+html = """
+    <!DOCTYPE html>
+    <html>
+        <body>
+            <h1 style="color:red;">This is an HTML Email!</h1>
+            <h2>{app.link_list[0]}</h2>
+        </body>
+    </html>
+    """
